6db7a0433e49374781daa1fd1c3f208861d681f0
emsquellen <24551188+emsquellen@users.noreply.github.com>
2021-04-07 20:40:21 +0200

    s

--- SB/models/__init__.py
+++ SB/models/__init__.py
@@ -12,7 +12,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    icon = db.Column(db.BLOB)
+    img = db.Column(db.BLOB)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(256))
